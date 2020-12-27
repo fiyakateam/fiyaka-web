84fa6910a76c42143c51ecf34072bf9de0d3db39
xinoip <pionix@protonmail.com>
2020-12-27 14:06:31 +0300

    router: add auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/view/login/login.component';
+import { LogoutComponent } from './auth/view/logout/logout.component';
+import { RegisterComponent } from './auth/view/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
