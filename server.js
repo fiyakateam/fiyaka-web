d747d00437484a754c9ce46f1840e986810239e9
xinoip <pionix@protonmail.com>
2020-12-27 13:37:16 +0300

    deployline: configure server.js for angular

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
+const appdir = __dirname + '/dist/fiyaka-webapp';
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(appdir));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(appdir + '/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
+});
