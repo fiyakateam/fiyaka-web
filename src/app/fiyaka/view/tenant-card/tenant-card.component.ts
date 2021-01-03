d7a2843e6dd9b310cf654ba87e9b023ee6ec939d
xinoip <pionix@protonmail.com>
2021-01-03 16:31:42 +0300

    feat/tenant: tenant card input tenant model

--- src/app/fiyaka/view/tenant-card/tenant-card.component.ts
+++ src/app/fiyaka/view/tenant-card/tenant-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Tenant } from '../../model/tenant.model';
 
 @Component({
   selector: 'app-tenant-card',
   templateUrl: './tenant-card.component.html',
-  styleUrls: ['./tenant-card.component.css']
+  styleUrls: ['./tenant-card.component.css'],
 })
 export class TenantCardComponent implements OnInit {
+  @Input() tenant: Tenant;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
