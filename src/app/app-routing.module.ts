c4f4b690c299d5556d861a3d250a54105cedc30b
xinoip <pionix@protonmail.com>
2021-01-03 17:55:10 +0300

    routing: use feat/auth-middleware guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/middleware/auth.guard';
+import { NonAuthGuard } from './auth/middleware/non-auth.guard';
 import { LoginComponent } from './auth/view/login/login.component';
 import { LogoutComponent } from './auth/view/logout/logout.component';
 import { RegisterComponent } from './auth/view/register/register.component';
+import { UnauthorizedComponent } from './auth/view/unauthorized/unauthorized.component';
 import { LandingComponent } from './core/view/landing/landing.component';
 import { NotFoundComponent } from './core/view/not-found/not-found.component';
 import { TenantsComponent } from './fiyaka/view/tenants/tenants.component';
@@ -10,10 +13,19 @@ import { TenantsComponent } from './fiyaka/view/tenants/tenants.component';
 const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
+    canActivate: [NonAuthGuard],
+  },
+  { path: 'login', component: LoginComponent, canActivate: [NonAuthGuard] },
   { path: 'logout', component: LogoutComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'tenants', component: TenantsComponent },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [NonAuthGuard],
+  },
+  { path: 'tenants', component: TenantsComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
 
