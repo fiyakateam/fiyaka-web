0252e2237eb41f06cdb167ad3faca97aacab4b86
xinoip <pionix@protonmail.com>
2021-01-03 16:26:40 +0300

    feat/tenant: create tenant card

---
+++ src/app/fiyaka/view/tenant-card/tenant-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tenant-card',
+  templateUrl: './tenant-card.component.html',
+  styleUrls: ['./tenant-card.component.css']
+})
+export class TenantCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
