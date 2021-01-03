160739c506d439766717f75f8603e797b60006f3
xinoip <pionix@protonmail.com>
2021-01-03 23:01:33 +0300

    feat/tenant: apipath

--- src/app/config/api-path.ts
+++ src/app/config/api-path.ts
@@ -1,8 +1,14 @@
 export class ApiPath {
-    private static readonly root: string = 'https://fiyakaapi.herokuapp.com';
-    private static readonly api: string = `${ApiPath.root}`;
+  private static readonly root: string = 'https://fiyakaapi.herokuapp.com';
+  private static readonly api: string = `${ApiPath.root}`;
 
-    public static readonly auth: string = `${ApiPath.api}/auth`;
-    public static readonly login: string = `${ApiPath.auth}/login`;
-    public static readonly register: string = `${ApiPath.auth}/register`;
+  public static readonly auth: string = `${ApiPath.api}/auth`;
+  public static readonly login: string = `${ApiPath.auth}/login`;
+  public static readonly register: string = `${ApiPath.auth}/register`;
+
+  public static readonly tenant: string = `${ApiPath.api}/tenant`;
+
+  public static tenantId(id: string): string {
+    return `${ApiPath.tenant}/${id}`;
+  }
 }
