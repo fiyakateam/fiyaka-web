92db447eeb8b007047fa6906df4384b1960c3f56
xinoip <pionix@protonmail.com>
2021-01-03 15:51:26 +0300

    feat/notification: add notification to module

--- src/app/auth/view/login/login.component.ts
+++ src/app/auth/view/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from 'src/app/core/service/notification.service';
 import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   isLogin = true;
@@ -16,32 +17,45 @@ export class LoginComponent implements OnInit {
   registerName = '';
   registerPassword = '';
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private notification: NotificationService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   switchToLogin(): void {
-    if (this.isLogin) { return; }
+    if (this.isLogin) {
+      return;
+    }
     this.isLogin = true;
   }
 
   switchToRegister(): void {
-    if (!this.isLogin) { return; }
+    if (!this.isLogin) {
+      return;
+    }
     this.isLogin = false;
   }
 
   onLogin(): void {
     console.warn('hello');
-    this.auth.login(this.loginEmail, this.loginPassword).subscribe((res) => {
-      console.log(res);
-    });
+    this.auth.login(this.loginEmail, this.loginPassword).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.error(err);
+        this.notification.pushError('Login failed');
+      }
+    );
   }
 
   onRegister(): void {
-    this.auth.register(this.registerName, this.registerEmail, this.registerPassword).subscribe((res) => {
-      console.log(res);
-    });
+    this.auth
+      .register(this.registerName, this.registerEmail, this.registerPassword)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
-
 }
