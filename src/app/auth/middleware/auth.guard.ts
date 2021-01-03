c3f88bd30e8f671b5388fdff3f9aca667a8c3c00
xinoip <pionix@protonmail.com>
2021-01-03 17:54:34 +0300

    feat/auth-middleware: create interceptors

---
+++ src/app/auth/middleware/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.auth.redirectUrl = state.url;
+    const authStatus = this.auth.authStatus();
+    if (authStatus.success) {
+      return true;
+    }
+
+    this.router.navigate(['/unauthorized']);
+    return false;
+  }
+}
---
+++ src/app/auth/middleware/non-auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../service/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NonAuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.auth.redirectUrl = state.url;
+    const authStatus = this.auth.authStatus();
+    if (authStatus.success) {
+      this.router.navigate(['/landing']);
+    }
+    return !authStatus.success;
+  }
+}
