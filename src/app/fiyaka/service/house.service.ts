eaf0c925f713316cdd54e5d4f851c81bceec9fdf
xinoip <pionix@protonmail.com>
2021-01-04 13:46:37 +0300

    feat/house: create service

---
+++ src/app/fiyaka/service/house.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiPath } from 'src/app/config/api-path';
+import { HouseResponse } from '../model/api/fiyaka_api';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HouseService {
+  constructor(private http: HttpClient) {}
+
+  getHouseList(): Observable<Array<HouseResponse>> {
+    return this.http.get<Array<HouseResponse>>(ApiPath.house);
+  }
+
+  getHouse(id: string): Observable<HouseResponse> {
+    return this.http.get<HouseResponse>(ApiPath.houseId(id));
+  }
+
+  deleteHouse(id: string): Observable<void> {
+    return this.http.delete<void>(ApiPath.houseId(id));
+  }
+
+  createHouse(house: HouseResponse): Observable<HouseResponse> {
+    const request = {
+      address: house.address,
+      name: house.name,
+    };
+    return this.http.post<HouseResponse>(ApiPath.house, request);
+  }
+
+  updateHouse(id: string, house: HouseResponse): Observable<HouseResponse> {
+    const request = {
+      address: house.address,
+      name: house.name,
+    };
+    return this.http.put<HouseResponse>(ApiPath.houseId(id), request);
+  }
+}
