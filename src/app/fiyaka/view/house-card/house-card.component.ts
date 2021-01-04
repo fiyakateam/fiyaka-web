084778fee7187d8af8709511e759c1a731da21ae
xinoip <pionix@protonmail.com>
2021-01-04 13:47:23 +0300

    feat/house: create house card

---
+++ src/app/fiyaka/view/house-card/house-card.component.ts
@@ -0,0 +1,50 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NotificationService } from 'src/app/core/service/notification.service';
+import { HouseResponse } from '../../model/api/fiyaka_api';
+import { HouseService } from '../../service/house.service';
+
+@Component({
+  selector: 'app-house-card',
+  templateUrl: './house-card.component.html',
+  styleUrls: ['./house-card.component.css'],
+})
+export class HouseCardComponent implements OnInit {
+  @Input() createPlaceholder: boolean;
+  @Input() house: HouseResponse;
+  @Output() createClick = new EventEmitter<void>();
+  @Output() updateClick = new EventEmitter<HouseResponse>();
+  @Output() deleted = new EventEmitter<void>();
+
+  constructor(
+    private tenantService: HouseService,
+    private notification: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    console.warn('hello: ');
+    console.warn(this.house);
+  }
+
+  onCreateClick(): void {
+    this.createClick.emit();
+  }
+
+  onDelete(): void {
+    this.tenantService.deleteHouse(this.house._id).subscribe(
+      (res) => {
+        this.notification.pushSuccess(`Deleted tenant ${this.house.name}`);
+        this.deleted.emit();
+      },
+      (err) => {
+        console.error(err);
+        this.notification.pushError(
+          `Could not delete tenant ${this.house.name}`
+        );
+      }
+    );
+  }
+
+  onUpdateClick(): void {
+    this.updateClick.emit(this.house);
+  }
+}
